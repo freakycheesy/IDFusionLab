4a779ada81e4112013711f5b4d3bf80dc78fcd1e
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-09-16 07:56:16 +0100

    Add project files.

---
+++ IDFusionLab/Core.cs
@@ -0,0 +1,37 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using LabFusion.Entities;
+using System.Reflection;
+using LabFusion.Player;
+
+[assembly: MelonInfo(typeof(IDFusionLab.Core), "IDFusionLab", "1.0.0", "pietr", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonAdditionalDependencies("LabFusion")]
+namespace IDFusionLab
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+            HarmonyInstance.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerID))]
+    public static class PlayerIDPatches {
+        private static Type PlayerType = typeof(PlayerID);
+        private static FieldInfo NameFieldInfo = PlayerType.GetField("_username", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [HarmonyPatch(nameof(PlayerID))]
+        [HarmonyPrefix]
+        public static void PlayerIDPrefix(PlayerID __instance) {
+            var metaData = __instance.Metadata;
+            var id = __instance.PlatformID;
+            var metaData = $"{__instance.Metadata.Username} \n({id})";
+            if (__instance.Metadata.Username != name) {
+                NameFieldInfo.SetValue(__instance, name);
+            }
+        }
+    }
+}
